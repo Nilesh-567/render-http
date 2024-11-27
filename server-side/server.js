2ca768565dbba3f0990e8a671a828299c555a053
Nilesh-567 <137327289+Nilesh-567@users.noreply.github.com>
2024-11-27 12:44:41 +0530

    Update server.js

--- server-side/server.js
+++ server-side/server.js
@@ -2,9 +2,12 @@ import axios from "axios";
 import cors from "cors";
 import express from "express";
 import path from "path";
+import dotenv from 'dotenv'; 
 import { Server } from "socket.io";
 import { fileURLToPath } from "url";
 import { checkConnection } from "./db.js"; // Ensure `db.js` is in the same directory
+
+dotenv.config();
 
 const app = express();
 const PORT = 3002;
@@ -31,7 +34,8 @@ const __dirname = path.dirname(__filename);
 app.get('/', async (req, res) => {
   try {
     // Fetch the HTML from Netlify
-    const response = await axios.get('https://mongodb-fetch-data.netlify.app');
+    const B_URL = 'https://mongodb-fetch-data.netlify.app'; 
+    const response = await axios.get(process.env.B_URL || B_URL);
     res.send(response.data); // Send the HTML to the client
   } catch (error) {
     console.error('Error fetching HTML:', error);
